ff154dbbd68b5492f6e2d0e347b93bac102ad18c
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-08 18:45:16 +0000

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_AXZ8M7M0RMzuoqunD8_zsj0g6n7om0I",
+    authDomain: "clone-975bf.firebaseapp.com",
+    databaseURL: "https://clone-975bf.firebaseio.com",
+    projectId: "clone-975bf",
+    storageBucket: "clone-975bf.appspot.com",
+    messagingSenderId: "515444531745",
+    appId: "1:515444531745:web:13b587eff69e54d0fe3960",
+    measurementId: "G-E5XPG8QFT7"
+  };
