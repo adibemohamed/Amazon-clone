1f54ed4758c274e8f46965a8bfe6de041e084b72
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-08 19:54:51 +0000

    Product comp:

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
             </div>
 
             <div className="home__row">
-                {/* Proc */}
+                
                  {/* Proc */}
             </div>
 
