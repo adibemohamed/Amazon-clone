8d224d65036a82687b27b9c2e7697df871ba661b
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-08 19:20:17 +0000

    style header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
-    <div className="app">
-      <h1>Boom</h1>
-
+    <div className="app"> 
 
       {/* Header */}
+      <Header />
 
-      {/* Home */} 
+      {/* Home */}
 
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import './Header.css'
+import 
 
 function Header() {
     return (
         <div className="header">
-            
+            <img className="header__logo" src="https://jordantravers.com/wp-content/uploads/2018/10/Amazon-Logo-1024x373.png" />
+
+            <div className="header__search">
+                <input
+                    type="text"
+                    className="header__searchInput" />
+                {/* Logo */}
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineTwo">Signin</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                </div>  
+                
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineTwo">Returns</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Orders</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineTwo">Returns</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Orders</span>
+                </div>
+
+            </div>
         </div>
     )
 }
