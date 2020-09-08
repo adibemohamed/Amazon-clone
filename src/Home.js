6d00bc1685492b0d75c8cc52deae9bb569e3afd9
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-08 19:53:49 +0000

    home products rows

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,24 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"></img>
+            </div>
+
+            <div className="home__row">
+                {/* Proc */}
+                 {/* Proc */}
+            </div>
+
+            <div className="home__row">
+                {/* Proc */}
+                {/* Proc */}
+                {/* Proc */}
+            </div>
+
+            <div className="home__row">
+                {/* Proc */} 
+            </div>
             
         </div>
     )
