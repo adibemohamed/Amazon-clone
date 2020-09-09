13811e68dcb4fdc8c6689b5e5d03e031eea294e1
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-09 20:26:07 +0000

    diptach logged in / logged out user to reducer state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import './App.css';
-import Header from './Header'
-import Home from './Home'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Checkout from './Checkout'
-import Login from './Login';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(`USER IS >>>> ${authUser}`);
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
-
         <Switch>
-
           <Route path="/checkout">
             <Header />
             <Checkout />
@@ -26,9 +48,7 @@ function App() {
             <Header />
             <Home />
           </Route>
-
         </Switch>
-
       </div>
     </Router>
   );
