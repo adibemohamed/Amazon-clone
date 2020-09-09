62710047c5094eba5331479cef1366609df508ee
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-09 00:15:31 +0000

    initialize the reducer

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,12 +1,14 @@
 import React, { createContext, useContext, useRecuder } from 'react';
 
-
+// Prepare the dataLayer
 export const StateContext = createContext();
 
+// Wrap our app and provide the data layer
 export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useRecuder(reducer, initialState)}>
         {children}
     </StateContext.Provider>
 )
 
+// Pull intormation from the data layer
 export const useStateValue = () => useContext(StateContext);
