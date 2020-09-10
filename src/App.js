a413a23b85a3d5012ce3ed541632fbc1c97a47de
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-10 19:26:23 +0000

    done style payment

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from './Payment';
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -42,7 +43,7 @@ function App() {
 
           <Route path="/payment">
             <Header /> 
-              <h1>I am the payment</h1>
+              <Payment />
           </Route>
 
           <Route path="/login">
