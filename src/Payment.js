e3c9620530f262a0ae21eb382033ec47155eba7c
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-11 17:01:22 +0000

    Format Currency + total in Stripe card

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 import { Link } from "react-router-dom";
-import { CardElement, useStripe, useElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+
+  const stirpe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = (e) => {
+    // do all fancy stripe suff
+  };
+
+  const handleChange = (e) => {
+    //listen for changes
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -50,13 +68,25 @@ function Payment() {
           </div>
         </div>
 
+        {/* Payment section - Payment method */}
         <div className="payment__section">
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
           <div className="payment__details">
-            <form>
-              <CardElement />
+            {/* Stripe magic will go */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)} //
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                ></CurrencyFormat>
+              </div>
             </form>
           </div>
         </div>
