3a43ad075ca667eed6074d0d6028820e6d551948
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-11 23:11:11 +0000

    SETUP express api

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HPvTrF1HgyCm2C01ukKpy6QYNIakPXUhaEJg9535aGqcbmCAZaC2LG1xcgH4LOD59M7VziNPdK4XKIBOktlA8TZ00cbJLKrO3"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Setup API
+
+// App config
+const app = express();
+
+// Middleware
+app.use(cors({ origin: true }))
+app.use(express.json())
+
+
+// API routes
+app.get('/', (request, response) => {
+    response.status(200).send('HELLO WOLRD');
+})
+
+
+// Listen command
+exports.api = functions.https.onRequest(app);
