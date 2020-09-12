7114496ba7f7d03dbdff53bef17a7a4e772cfd81
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-12 01:20:59 +0000

    ADD Order compo

--- src/Orders.js
+++ src/Orders.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import './Orders.css';
+import React, { useEffect, useState } from 'react'
+import './Orders.css'; 
+import {db} from './firebase';
+import {useStateValue} from './StateProvider';
 
 function Orders() {
+    const [{basket, user}, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(()=>{
+        db
+            .collection('orders')
+            .
+
+    }, [])
+
     return (
         <div className="orders">
             
