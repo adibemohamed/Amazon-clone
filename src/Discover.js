84a4de4710ec90b9b986ab90e839a47450ea8e39
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-19 19:56:01 +0000

    ADD Discover compo/section

---
+++ src/Discover.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Discover() {
+    return (
+        <div className="discover">
+            <h1>I am the the discoer</h1>
+        </div>
+    )
+}
+
+export default Discover
